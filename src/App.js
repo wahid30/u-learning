94a8a477753d010eab2869a5684b314065e3ce30
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-24 23:37:56 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { routes } from "./routes/Routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Shared/Header/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Main;
