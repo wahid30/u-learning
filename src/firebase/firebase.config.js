05002272ac1a02922408f2bebe04a6cef6b26f40
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-26 15:42:45 +0600

    firebase configuration and export app

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKFJz3c2gLX66GLWuNegOmwbL7yQuyNWw",
+  authDomain: "u-learning-b759b.firebaseapp.com",
+  projectId: "u-learning-b759b",
+  storageBucket: "u-learning-b759b.appspot.com",
+  messagingSenderId: "451642556394",
+  appId: "1:451642556394:web:6cdf80b8493494ae847c93",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
