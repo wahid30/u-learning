ae2a7504c137c5c0f3a81d2afb0b50af5d570883
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-26 15:41:17 +0600

    firebase install

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,9 +1,11 @@
 import React, { createContext } from "react";
+import { useState } from "react";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const user = {};
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
   const authInfo = { user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
