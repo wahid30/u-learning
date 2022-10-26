d21282b7b7fe0ffc450366f5b643b9b1b51a3df7
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-26 15:57:45 +0600

    firebase configuration and export ap

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,12 +1,43 @@
 import React, { createContext } from "react";
 import { useState } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { useEffect } from "react";
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(true);
-  const authInfo = { user };
+
+  // for registration
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // for login
+  const singIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, loading, createUser, singIn };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,7 +4,14 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthProvider";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading.........</h2>
+      </div>
+    );
+  }
   if (user && user.uid) {
     return children;
   }
